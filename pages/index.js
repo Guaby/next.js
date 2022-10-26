fd4160092b49b2c4c289fa00d9de541c14e59494
Gabriel Rosario <guaby18@gmail.com>
2022-10-26 18:36:00 -0400

    Framer Handshake with Next.js
    Starting the project

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
-import { Tip } from "../components/Tip";
+// import { Tip } from "../components/Tip";
 import { Container } from "../components/Container";
 
+// import Button from "https://framer.com/m/Button-vA04.js"
+// import Table from "https://framer.com/m/Table-VYAj.js@0YxgSwy7luCHT63Mz04Z"
+import FeatureWindow from "https://framer.com/m/Feature-window-ekJs.js"
+
 // A Smart Component from Framer
-import Toggle from "https://framer.com/m/Toggle-B5iT.js@52zFaz7rN7Bt3pjtYxWH";
+// import Toggle from "https://framer.com/m/Toggle-B5iT.js@52zFaz7rN7Bt3pjtYxWH";
 
 export default function Home() {
   return (
@@ -15,8 +19,9 @@ export default function Home() {
       </Head>
 
       <Container>
-        <Toggle />
-        <Tip />
+        {/* <Button /> */}
+        {/* <Table /> */}
+        <FeatureWindow />
       </Container>
     </>
   );
